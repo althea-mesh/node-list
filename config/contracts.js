621385963010a897531a113721ddf7f4cae359e0
Sebastian Bolanos <sebasheston@gmail.com>
2019-02-23 12:38:46 -0600

    Working rinkeby deploy

--- config/contracts.js
+++ config/contracts.js
@@ -3,28 +3,29 @@ module.exports = {
   default: {
     // Blockchain node to deploy the contracts
     deployment: {
-      host: 'localhost', // Host of the blockchain node
+      host: "localhost", // Host of the blockchain node
       port: 8546, // Port of the blockchain node
-      type: 'ws', // Type of connection (ws or rpc),
+      type: "ws", // Type of connection (ws or rpc),
       // Accounts to use instead of the default account to populate your wallet
       // The order here corresponds to the order of `web3.eth.getAccounts`, so the first one is the `defaultAccount`
       accounts: [
         {
-          mnemonic: 'cook mango twist then skin sort option civil have still rather guilt',
-          addressIndex: '0', // Optionnal. The index to start getting the address
+          mnemonic:
+            "cook mango twist then skin sort option civil have still rather guilt",
+          addressIndex: "0", // Optionnal. The index to start getting the address
           numAddresses: "10", // Optional. The number of addresses to get
           hdpath: "m/44'/60'/0'/0/" // Optionnal. HD derivation path
         },
         {
-          'nodeAccounts': true // Uses the Ethereum node's accounts
+          nodeAccounts: true // Uses the Ethereum node's accounts
         }
       ]
     },
     // order of connections the dapp should connect to
     dappConnection: [
-      '$WEB3', // uses pre existing web3 object if available (e.g in Mist)
-      'ws://localhost:8546',
-      'http://localhost:8545'
+      "$WEB3", // uses pre existing web3 object if available (e.g in Mist)
+      "ws://localhost:8546",
+      "http://localhost:8545"
     ],
 
     // Automatically call `ethereum.enable` if true.
@@ -32,7 +33,7 @@ module.exports = {
     // Default value is true.
     // dappAutoEnable: true,
 
-    gas: 'auto',
+    gas: "auto",
 
     // Strategy for the deployment of the contracts:
     // - implicit will try to deploy all the contracts located inside the contracts directory
@@ -45,10 +46,10 @@ module.exports = {
     contracts: {
       SafeMath: {},
       MultiSigWallet: {
-        args: [['$accounts[0]'], 1]
+        args: [["$accounts[0]"], 1]
       },
       Althea: {
-        args: ['$MultiSigWallet'],
+        args: ["$MultiSigWallet"],
         onDeploy: [
           `Althea.methods.addMember('$accounts[1]', '0x2001deadbeefbf0aa000000000000000', '0x09C4D1F918D3C02B390765C7EB9849842c8F7997').send()`,
           `Althea.methods.addMember('$accounts[2]', '0x2001deadbeefbf0d0000000000000000', '0x4465626f726168000000000000000000').send()`,
@@ -65,29 +66,19 @@ module.exports = {
   // assumed to be the intended environment by `embark run`
   development: {
     dappConnection: [
-      'ws://localhost:8546',
-      'http://localhost:8545',
-      '$WEB3' //  uses pre existing web3 object if available (e.g in Mist)
+      "ws://localhost:8546",
+      "http://localhost:8545",
+      "$WEB3" //  uses pre existing web3 object if available (e.g in Mist)
     ]
   },
 
-  // merges with the settings in default
-  // used with "embark run privatenet"
-  privatenet: {
-  },
-
-  // merges with the settings in default
-  // used with "embark run testnet"
-  testnet: {
-  },
-
-  // merges with the settings in default
-  // used with "embark run livenet"
-  livenet: {
+  rinkeby: {
+    deployment: {
+      accounts: [{ mnemonic: process.env.MNEMONIC }],
+      host: "rinkeby.infura.io/v3/" + process.env.INFURA_API,
+      port: false,
+      protocol: "https",
+      type: "rpc"
+    }
   }
-
-  // you can name an environment with specific settings and then specify with
-  // "embark run custom_name" or "embark blockchain custom_name"
-  // custom_name: {
-  // }
 };
